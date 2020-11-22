88a1207a072e596c526f4f261d97ee5cb0f5893c
Johannes Fagerberg <johfa464@student.liu.se>
2020-11-22 16:06:42 +0100

    Updates

---
+++ component---src-pages-index-js-30acf6b6104003e08cc3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{RXBc:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),r=(a("Wbzz"),a("MrVB"),a("vrFN"),a("uxxp")),c=a("3FwE");a("ZuEc");t.default=function(){return l.a.createElement("div",{className:"wrapper"},l.a.createElement(r.a,null),l.a.createElement(c.a,{title:"Portfolio"}),l.a.createElement(c.a,{title:"TankBattles"}),l.a.createElement(c.a,{title:"CatBook"}),l.a.createElement(c.a,{title:"CodEng"}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-30acf6b6104003e08cc3.js.map
